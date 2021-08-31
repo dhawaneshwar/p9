8ddd240ce89bff4a557871bb7f2ef5f86e642114
dhawaneshwar <89082879+dhawaneshwar@users.noreply.github.com>
2021-08-31 18:19:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background("yellow"); 
+  if (keyWentDown(UP_ARROW)){
+    background("red")
+  } 
+  drawSprites();
+  
+}
